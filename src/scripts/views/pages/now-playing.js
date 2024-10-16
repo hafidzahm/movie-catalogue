d8b556c738bc46e5b8facde7307c74476507ab51
Abdul <hafizhahm123@gmail.com>
2024-10-17 05:53:49 +0700

    add: page

---
+++ src/scripts/views/pages/now-playing.js
@@ -0,0 +1,16 @@
+const NowPlaying = {
+  async render() {
+    return `
+        <h2>Now Playing Page</h2>
+        `;
+
+
+  },
+
+  async afterRender() {
+    // Fungsi ini akan dipanggil setelah render()
+  },
+
+};
+
+export default NowPlaying;
