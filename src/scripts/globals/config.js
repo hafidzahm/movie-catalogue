a93c8098265096d08e2bdf59f7c82fb2a7e54bb2
Abdul <hafizhahm123@gmail.com>
2024-10-18 06:05:36 +0700

    update: API CONFIG

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  KEY: 'YOUR_API_KEY',
+  KEY: '8521ab53bdb2c8d2b7362d4f5946ff92',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
