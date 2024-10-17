a769c854ba0aa20706f17723552e6dda4f094717
Abdul <hafizhahm123@gmail.com>
2024-10-17 09:23:46 +0700

    test

--- src/scripts/data/themoviedb-source.js
+++ src/scripts/data/themoviedb-source.js
@@ -4,7 +4,7 @@ class TheMovieDbSource {
   static async nowPlayingMovies() {
     const response = await fetch(API_ENDPOINT.NOW_PLAYING);
     const responseJson = await response.json();
-    return responseJson.results;
+    return responseJson.result;
   }
 
   static async upcomingMovies() {
