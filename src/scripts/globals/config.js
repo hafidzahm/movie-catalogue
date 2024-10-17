4db9576e601e269d0f961ec0c5ce58354f215965
Abdul <hafizhahm123@gmail.com>
2024-10-17 08:52:58 +0700

    config-API

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,12 +1,8 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
-
 const CONFIG = {
-  KEY: process.env.YOUR_API_KEY,
-  BASE_URL: process.env.BASE_URL,
-  BASE_IMAGE_URL: process.env.BASE_IMAGE_URL,
-  DEFAULT_LANGUAGE: process.env.DEFAULT_LANGUAGE,
-
+  KEY: '8521ab53bdb2c8d2b7362d4f5946ff92',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
 };
 
 export default CONFIG;
