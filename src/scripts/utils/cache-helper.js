8c7ed5ea1acb48e537a92f3aab478a1ebe3ff536
Abdul <hafizhahm123@gmail.com>
2024-10-18 05:55:28 +0700

    lengkapi fungsi revalidateCache(). Fungsi ini membutuhkan fungsi kecil lain yakni melakukan fetch request dan memasukkan request ke dalam cache. Sehingga buat dulu dua fungsi _fetchRequest() dan _addCache()

--- src/scripts/utils/cache-helper.js
+++ src/scripts/utils/cache-helper.js
@@ -11,12 +11,26 @@ const CacheHelper = {
       .map((filteredName) => caches.delete(filteredName));
   },
 
-  async revalidateCache(request) {
-
-  },
+  async revalidateCache(request) {},
 
   async _openCache() {
     return caches.open('MovieCatalogue-V1');
+  },
+
+  async _fetchRequest(request) {
+    const response = await fetch(request);
+
+    if (!response || response.status !== 200) {
+      return response;
+    }
+
+    await this._addCache(request);
+    return response;
+  },
+
+  async _addCache(request) {
+    const cache = await this._openCache();
+    cache.add(request);
   },
 };
 
