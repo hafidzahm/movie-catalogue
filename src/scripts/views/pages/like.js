7b940078cd748ced587948fc6686dbacbf2b0bd4
Abdul <hafizhahm123@gmail.com>
2024-10-21 05:46:40 +0700

    Menampilkan Data dari Database: Likes Page

--- src/scripts/views/pages/like.js
+++ src/scripts/views/pages/like.js
@@ -1,6 +1,13 @@
 const Like = {
   async render() {
-
+    return `
+    <div class="content">
+      <h2 class="content__heading">Your Liked Movie</h2>
+      <div id="movies" class="movies">
+ 
+      </div>
+    </div>
+  `;
   },
 
   async afterRender() {
