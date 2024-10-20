8237ec9bb2fd52132cbb6d57e2d86e8062302cbd
Abdul <hafizhahm123@gmail.com>
2024-10-20 17:15:42 +0700

    Menambahkan Tombol Like pada Halaman Detail

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,11 +1,12 @@
 import UrlParser from '../../routes/url-parser';
 import TheMovieDbSource from '../../data/themoviedb-source';
-import { createMovieDetailTemplate } from '../templates/template-creator';
+import { createMovieDetailTemplate, createLikeButtonTemplate } from '../templates/template-creator';
 
 const Detail = {
   async render() {
     return `
     <div id="movie" class="movie"></div>
+    <div id="likeButtonContainer"></div>
         `;
   },
   async afterRender() {
@@ -16,7 +17,9 @@ const Detail = {
 
     // TODO: tampilkan movie di dalam DOM
     const movieContainer = document.querySelector('#movie');
+    const likeButtonContainer = document.querySelector('#likeButtonContainer');
     movieContainer.innerHTML = createMovieDetailTemplate(movie);
+    likeButtonContainer.innerHTML = createLikeButtonTemplate();
   },
 };
 
