61827505d62a52a08002216c939beedba6494f8b
Abdul <hafizhahm123@gmail.com>
2024-10-21 16:21:05 +0700

    Sebelum mengirimkan notifikasi, diperlukan beberapa tahap yang harus dilewati sebagai berikut.
    1.  Pengecekan apakah browser mendukung notifikasi.
    2.  Pengecekan apakah fitur notifikasi diijinkan. Jika belum, lakukan permohonan perizinan.
    3. Menampilkan notifikasi.
    
    Untuk itu, kita perlu membuat fungsi-fungsi kecilnya terlebih dahulu. Buatlah fungsi-fungsi seperti berikut.

--- src/scripts/utils/notification-helper.js
+++ src/scripts/utils/notification-helper.js
@@ -1,8 +1,37 @@
 const NotificationHelper = {
   sendNotification({ title, options }) {
+    // TODO: check availability
 
-  }
+    // TODO: check permissions. if not granted, send request
 
+    // TODO: show notification
+
+  },
+
+  _checkAvailability() {
+    return 'Notification' in window;
+  },
+
+  _checkPermission() {
+    return Notification.permission === 'granted';
+  },
+
+  async _requestPermission() {
+    const status = await Notification.requestPermission();
+
+    if (status === 'denied') {
+      console.log('Notification Denied');
+    }
+
+    if (status === 'default') {
+      console.log('Permission closed');
+    }
+  },
+
+  async _showNotification({ title, options }) {
+    const serviceWorkerRegistration = await navigator.serviceWorker.ready;
+    serviceWorkerRegistration.showNotification(title, options);
+  },
 };
 
 export default NotificationHelper;
